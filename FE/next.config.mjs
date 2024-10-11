45855d2756d343ed3933658486498c6e4783d5b5
Pedro Lucas <132102192+pedromourax@users.noreply.github.com>
2024-10-11 02:01:48 -0300

    Update next.config.mjs

--- FE/next.config.mjs
+++ FE/next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "http://backend:3000/:path*",
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
